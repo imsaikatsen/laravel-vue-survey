1e5fca1577b2da3ef6705c866c441803b0dff0b7
imsaikatsen <sensaikat100@gmail.com>
2022-07-31 06:03:06 +0600

    design done

--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -4,14 +4,38 @@ import Surveys from '../views/Surveys.vue'
 import Login from '../views/Login.vue'
 import Register from '../views/Register.vue'
 import DefaultLayout from '../components/DefaultLayout.vue'
+import AuthLayout from '../components/AuthLayout.vue'
+import store from "../store";
 const routes = [
     {
         path: '/',
         redirect: '/dashboard',
         component: DefaultLayout,
+        meta: {requiresAuth: true},
         children: [
             {path: '/dashboard', name: 'Dashboard', component: Dashboard},
-            {path: '/surveys', name: 'surveys', component: Surveys}
+            {path: '/surveys', name: 'Surveys', component: Surveys}
+        ]
+    },
+
+    {
+        path: '/auth',
+        redirect: '/login',
+        name: 'Auth',
+        component: AuthLayout,
+        meta: {isGuest: true},
+        children: [
+            {
+                path: '/login',
+                name: 'Login',
+                component: Login
+
+            },
+            {
+                path: '/register',
+                name: 'Register',
+                component: Register
+            },
         ]
     },
     {
@@ -30,6 +54,16 @@ const routes = [
 const router = createRouter({
     history: createWebHistory(),
     routes
+})
+
+router.beforeEach((to , from , next) => {
+    if (to.meta.requiresAuth && !store.state.user.token){
+        next({name:'Login'})
+    }else if (store.state.user.token && (to.meta.isGuest)) {
+        next({name: 'Dashboard'})
+    } else{
+        next();
+    }
 })
 
 export default router;
