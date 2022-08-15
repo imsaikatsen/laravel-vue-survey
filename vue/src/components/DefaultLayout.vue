8a82bb0dd1716dd7b4b3b07c42dbdd3dc72b633c
imsaikatsen <sensaikat100@gmail.com>
2022-08-15 17:57:44 +0600

    question form done

--- vue/src/components/DefaultLayout.vue
+++ vue/src/components/DefaultLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="min-h-full w-full">
+  <div class="min-h-full">
     <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
       <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex items-center justify-between h-16">
@@ -24,56 +24,47 @@
                       : 'text-gray-300 hover:bg-gray-700 hover:text-white',
                     'px-3 py-2 rounded-md text-sm font-medium',
                   ]"
-                  >{{ item.name }}</router-link
-                >
+                  >{{ item.name }}
+                </router-link>
               </div>
             </div>
           </div>
           <div class="hidden md:block">
             <div class="ml-4 flex items-center md:ml-6">
-              <button
-                type="button"
-                class="
-                  bg-gray-800
-                  p-1
-                  rounded-full
-                  text-gray-400
-                  hover:text-white
-                  focus:outline-none
-                  focus:ring-2
-                  focus:ring-offset-2
-                  focus:ring-offset-gray-800
-                  focus:ring-white
-                "
-              >
-                <span class="sr-only">View notifications</span>
-                <BellIcon class="h-6 w-6" aria-hidden="true" />
-              </button>
-
               <!-- Profile dropdown -->
               <Menu as="div" class="ml-3 relative">
-                <div>
+                <div class="flex">
                   <MenuButton
-                    class="
-                      max-w-xs
-                      bg-gray-800
-                      rounded-full
-                      flex
-                      items-center
-                      text-sm
-                      focus:outline-none
-                      focus:ring-2
-                      focus:ring-offset-2
-                      focus:ring-offset-gray-800
-                      focus:ring-white
-                    "
+                    class="max-w-xs bg-gray-800 rounded-full flex items-center text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                   >
                     <span class="sr-only">Open user menu</span>
-                    <img
-                      class="h-8 w-8 rounded-full"
-                      :src="user.imageUrl"
-                      alt=""
-                    />
+
+                    <div class="mx-3">
+                      <div
+                        class="text-left text-base font-medium leading-none text-white"
+                      >
+                        {{ user.name }}
+                      </div>
+                      <div
+                        class="text-sm font-medium leading-none text-gray-400"
+                      >
+                        {{ user.email }}
+                      </div>
+                    </div>
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      class="h-8 w-8"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="white"
+                    >
+                      <path
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                        stroke-width="2"
+                        d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                      />
+                    </svg>
                   </MenuButton>
                 </div>
                 <transition
@@ -85,21 +76,9 @@
                   leave-to-class="transform opacity-0 scale-95"
                 >
                   <MenuItems
-                    class="
-                      origin-top-right
-                      absolute
-                      right-0
-                      mt-2
-                      w-48
-                      rounded-md
-                      shadow-lg
-                      py-1
-                      bg-white
-                      ring-1 ring-black ring-opacity-5
-                      focus:outline-none
-                    "
+                    class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none"
                   >
-                    <MenuItem>
+                    <MenuItem v-slot="{}">
                       <a
                         @click="logout"
                         :class="[
@@ -116,21 +95,7 @@
           <div class="-mr-2 flex md:hidden">
             <!-- Mobile menu button -->
             <DisclosureButton
-              class="
-                bg-gray-800
-                inline-flex
-                items-center
-                justify-center
-                p-2
-                rounded-md
-                text-gray-400
-                hover:text-white hover:bg-gray-700
-                focus:outline-none
-                focus:ring-2
-                focus:ring-offset-2
-                focus:ring-offset-gray-800
-                focus:ring-white
-              "
+              class="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
             >
               <span class="sr-only">Open main menu</span>
               <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
@@ -145,7 +110,6 @@
           <router-link
             v-for="item in navigation"
             :key="item.name"
-            as="a"
             :to="item.to"
             active-class="bg-gray-900 text-white"
             :class="[
@@ -154,13 +118,26 @@
                 : 'text-gray-300 hover:bg-gray-700 hover:text-white',
               'block px-3 py-2 rounded-md text-base font-medium',
             ]"
-            >{{ item.name }}</router-link
-          >
+            >{{ item.name }}
+          </router-link>
         </div>
         <div class="pt-4 pb-3 border-t border-gray-700">
           <div class="flex items-center px-5">
             <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" :src="user.imageUrl" alt="" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                class="h-8 w-8"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="white"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                />
+              </svg>
             </div>
             <div class="ml-3">
               <div class="text-base font-medium leading-none text-white">
@@ -170,51 +147,22 @@
                 {{ user.email }}
               </div>
             </div>
-            <button
-              type="button"
-              class="
-                ml-auto
-                bg-gray-800
-                flex-shrink-0
-                p-1
-                rounded-full
-                text-gray-400
-                hover:text-white
-                focus:outline-none
-                focus:ring-2
-                focus:ring-offset-2
-                focus:ring-offset-gray-800
-                focus:ring-white
-              "
-            >
-              <span class="sr-only">View notifications</span>
-              <BellIcon class="h-6 w-6" aria-hidden="true" />
-            </button>
           </div>
           <div class="mt-3 px-2 space-y-1">
             <DisclosureButton
               as="a"
               @click="logout"
-              :href="item.href"
-              class="
-                block
-                px-3
-                py-2
-                rounded-md
-                text-base
-                font-medium
-                text-gray-400
-                hover:text-white hover:bg-gray-700
-                cursor-pointer
-              "
-              >Sign out</DisclosureButton
-            >
+              class="block px-3 py-2 rounded-md text-base font-medium text-gray-400 hover:text-white hover:bg-gray-700 cursor-pointer"
+              >Sign out
+            </DisclosureButton>
           </div>
         </div>
       </DisclosurePanel>
     </Disclosure>
 
-    <router-view></router-view>
+    <router-view :key="$route.path"></router-view>
+
+    <Notification />
   </div>
 </template>
 
--- vue/src/router/index.js
+++ vue/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Dashboard from '../views/Dashnoard.vue';
 import Surveys from '../views/Surveys.vue';
+import SurveyView from '../views/SurveyView.vue';
 import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 import DefaultLayout from '../components/DefaultLayout.vue';
@@ -15,6 +16,8 @@ const routes = [
     children: [
       { path: "/dashboard", name: "Dashboard", component: Dashboard },
       { path: "/surveys", name: "Surveys", component: Surveys },
+      { path: "/surveys/create", name: "SurveyCreate", component: SurveyView },
+      { path: "/surveys/:id", name: "SurveyView", component: SurveyView },
     ],
   },
   {
